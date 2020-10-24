b8be0e91d6baccdada5536e4871b72eb3f96fc5c
Starlight Romero <starlightromero@gmail.com>
2020-10-24 08:43:14 -0700

    added inputs and outbuts with two way binding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import UserInput from './UserInput/UserInput'
+import UserOutput from './UserOutput/UserOutput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    username: 'superstar'
+  }
+
+  usernameChangedHandler = event => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <UserInput changed={this.usernameChangedHandler} currentName={this.state.username} />
+        <UserOutput username='Sid' />
+        <UserOutput username={this.state.username} />
+        <UserOutput username='Bill' />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
